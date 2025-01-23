fb68f84666564b71edc72163ef28928b964ba226
Ronak99 <punase.ronak99@gmail.com>
2025-01-23 15:46:51 +0530

    Created star rush background

---
+++ src/majestic_star_rush_background/lib/majestic_star_rush_background.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MajesticStarRushBackground extends StatefulWidget {
+  const MajesticStarRushBackground({super.key});
+
+  @override
+  State<MajesticStarRushBackground> createState() =>
+      _MajesticStarRushBackgroundState();
+}
+
+class _MajesticStarRushBackgroundState
+    extends State<MajesticStarRushBackground> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Majestic Star Rush Background");
+  }
+}
