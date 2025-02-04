fe69fefadeb50a967c71efd2604cbf65382e60a6
Ronak99 <punase.ronak99@gmail.com>
2025-02-04 12:56:27 +0530

    Updated profile page demo

---
+++ demo/lib/profile_page_demo.dart
@@ -0,0 +1,32 @@
+import 'package:demo/majestic/ui/onboarding_page/onboarding_page.dart';
+import 'package:demo/majestic/ui/profile_page/profile_page.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePageDemo extends StatelessWidget {
+  const ProfilePageDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfilePage();
+  }
+}
+
+// Or Navigate to this page
+class ParentPage extends StatelessWidget {
+  const ParentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ProfilePage(),
+          ),
+        ),
+        child: const Text('Navigate'),
+      ),
+    );
+  }
+}
